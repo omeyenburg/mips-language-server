c67fcbe08992bc28ab6b53971496d852cb6cba2a
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2025-01-21 13:37:01 +0100

    feat(hover): improved structure

--- src/main.rs
+++ src/main.rs
@@ -269,14 +269,15 @@ impl LanguageServer for Backend {
                 .map(|keyword| CompletionItem {
                     label: keyword.to_string(),
                     kind: Some(CompletionItemKind::KEYWORD),
-                    detail: Some(format!(
-                        "MIPS directive: {}",
-                        self.directives.get(keyword).unwrap()
-                    )),
+                    detail: Some(format!("MIPS directive: {}", keyword)),
                     label_details: Some(CompletionItemLabelDetails {
                         detail: None,
                         description: Some("directive".to_string()),
                     }),
+                    documentation: Some(Documentation::MarkupContent(MarkupContent {
+                        kind: MarkupKind::Markdown,
+                        value: self.directives.get(keyword).unwrap().to_string(),
+                    })),
                     text_edit: Some(CompletionTextEdit::Edit(TextEdit {
                         range: Range {
                             start: Position {
@@ -290,8 +291,10 @@ impl LanguageServer for Backend {
                     ..Default::default()
                 })
                 .collect(),
+
             '$' => {
                 let mut registers_common_float = self.registers.common.clone();
+
                 let registers = if character - starting_index == 1 {
                     // Return common & float
                     for (key, value) in &self.registers.float {
@@ -299,7 +302,7 @@ impl LanguageServer for Backend {
                     }
                     &registers_common_float
                 } else if let Some(char) = line_content.chars().nth(starting_index as usize + 1) {
-                    // check_context
+                    // Check context
                     match char {
                         '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
                             &self.registers.numeric
@@ -314,19 +317,21 @@ impl LanguageServer for Backend {
                     }
                     &registers_common_float
                 };
+
                 registers
                     .keys()
                     .map(|keyword| CompletionItem {
                         label: keyword.to_string(),
                         kind: Some(CompletionItemKind::VALUE),
-                        detail: Some(format!(
-                            "MIPS register: {}",
-                            registers.get(keyword).unwrap()
-                        )),
+                        detail: Some(format!("MIPS register: {}", keyword)),
                         label_details: Some(CompletionItemLabelDetails {
                             detail: None,
                             description: Some("register".to_string()),
                         }),
+                        documentation: Some(Documentation::MarkupContent(MarkupContent {
+                            kind: MarkupKind::Markdown,
+                            value: registers.get(keyword).unwrap().to_string(),
+                        })),
                         text_edit: Some(CompletionTextEdit::Edit(TextEdit {
                             range: Range {
                                 start: Position {
@@ -341,19 +346,27 @@ impl LanguageServer for Backend {
                     })
                     .collect()
             }
+
             _ => {
                 let good_named_func = |keyword: &str| CompletionItem {
                     label: keyword.to_string(),
                     kind: Some(CompletionItemKind::KEYWORD),
-                    detail: Some("keyword".to_string()),
+                    detail: Some({
+                        let instruction_format = &self.instructions.get(keyword).unwrap().format;
+                        if instruction_format.is_empty() {
+                            format!("Pseudo Instruction: {}", keyword)
+                        } else {
+                            format!("{} Format Instruction: {}", instruction_format, keyword)
+                        }
+                    }),
                     label_details: Some(CompletionItemLabelDetails {
                         detail: None,
-                        description: Some("keyword".to_string()),
+                        description: Some("instruction".to_string()),
                     }),
                     documentation: Some(Documentation::MarkupContent(MarkupContent {
                         kind: MarkupKind::Markdown,
-                        value: self.get_instruction_docs(
-                            keyword.to_string(),
+                        value: self.short_instruction_docs(
+                            keyword,
                             self.instructions.get(keyword).unwrap(),
                         ),
                     })),
@@ -371,7 +384,7 @@ impl LanguageServer for Backend {
                 };
 
                 // Completion is buggy when there is a dot.
-                // This should fix this
+                // This should filter instruction completions.
                 if let Some(dot_position) = line_content[..character as usize].rfind(".") {
                     let prefix = &line_content[starting_index as usize..character as usize];
                     self.instructions
@@ -435,7 +448,7 @@ impl LanguageServer for Backend {
             "opcode" => {
                 if let Some(instruction) = self.instructions.get(cursor_node_text) {
                     let documentation =
-                        self.get_instruction_docs(cursor_node_text.to_string(), instruction);
+                        self.long_instruction_docs(cursor_node_text.to_string(), instruction);
 
                     return Ok(Some(Hover {
                         contents: HoverContents::Scalar(MarkedString::String(documentation)),
@@ -606,79 +619,61 @@ impl Backend {
         diagnostics
     }
 
-    /*
-    Returns a description of an instruction as a String.
-    Format:
-    [...] Type Instruction
-    –––
-    ```asm
-    [opcode] [operands]
-    ```
-    [description]
-    [machine code]
-    –––
-    ```asm
-    [opcode] [operands]
-    ```
-    [description]
-    [machine code]
-    */
-    fn get_instruction_docs(&self, opcode: String, instruction: &json::Instruction) -> String {
-        if instruction.format.is_empty() {
-            // Pure pseudo instruction
-            let mut docs = "# Pseudo Instruction\n---\n".to_string();
+    /**
+     * Returns a long description of an instruction as a String.
+     * Prepends a short description with the instruction format.
+     */
+    fn long_instruction_docs(&self, opcode: String, instruction: &json::Instruction) -> String {
+        let instruction_format = &instruction.format;
+        format!(
+            "{}: `{}`\n---\n{}",
+            if instruction_format.is_empty() {
+                "Pseudo Instruction".to_string()
+            } else {
+                format!("{} Format Instruction", instruction_format)
+            },
+            opcode,
+            self.short_instruction_docs(opcode.as_str(), instruction)
+        )
+    }
 
-            for variant in &instruction.pseudo {
-                docs = format!(
-                    "{}\n```asm\n{} {}\n```\n{}\n",
-                    docs,
-                    opcode,
-                    variant.operands.join(", "),
-                    variant.description
-                );
-            }
-            docs
-        } else if instruction.pseudo.is_empty() {
-            // Pure native instruction
-            let mut docs = format!("# {} Format Instruction\n", instruction.format);
+    /**
+     * Returns a short description of an instruction as a String.
+     */
+    fn short_instruction_docs(&self, opcode: &str, instruction: &json::Instruction) -> String {
+        let native_instruction = !instruction.native.is_empty();
+        let pseudo_instruction = !instruction.pseudo.is_empty();
 
+        let mut docs = "".to_string();
+
+        if native_instruction {
             for variant in &instruction.native {
                 docs = format!(
-                    "{}\n```asm\n{} {}\n```\n{}\n",
+                    "{}\n{}\n```asm\n{} {}\n```\n",
                     docs,
+                    variant.description,
                     opcode,
                     variant.operands.join(", "),
-                    variant.description
                 );
             }
-            docs
-        } else {
-            // Mixed instruction
-            let mut docs = format!("# {} Format Instruction\n", instruction.format);
-
-            for variant in &instruction.native {
-                docs = format!(
-                    "{}\n```asm\n{} {}\n```\n{}\n",
-                    docs,
-                    opcode,
-                    variant.operands.join(", "),
-                    variant.description
-                );
-            }
-
-            docs = format!("{}## Pseudo Instruction Alternative\n", docs);
-
-            for variant in &instruction.pseudo {
-                docs = format!(
-                    "{}\n```asm\n{} {}\n```\n{}\n",
-                    docs,
-                    opcode,
-                    variant.operands.join(", "),
-                    variant.description
-                );
-            }
-            docs
         }
+
+        if pseudo_instruction {
+            if native_instruction {
+                docs = format!("{}\n---\n**Pseudo Instruction Alternative**\n&nbsp;", docs);
+            }
+
+            for variant in &instruction.pseudo {
+                docs = format!(
+                    "{}\n{}\n```asm\n{} {}\n```",
+                    docs,
+                    variant.description,
+                    opcode,
+                    variant.operands.join(", "),
+                );
+            }
+        }
+        docs
     }
 }
 
