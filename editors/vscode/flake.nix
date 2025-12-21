a45cc2dc8b20a69568427bfddf977af403f6e75e
omeyenburg <omeyenburg@gmail.com>
2025-12-21 10:51:58 +0100

    chore: create nix flake for vscode bridge plugin

---
+++ editors/vscode/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "VSCode flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            vscode
+          ];
+
+          shellHook = ''
+            export SHELL=${pkgs.bashInteractive}/bin/bash
+          '';
+        };
+      }
+    );
+}
